b9f9fabe7105454f0eb851a4093786f5891e5a21
AmerBellioum <abellioum14@gmail.com>
2025-06-17 15:01:56 +0100

    Update hedge_book.py

--- src/hedge_book.py
+++ src/hedge_book.py
@@ -1,18 +1,10 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jun 11 12:22:54 2025
-
-@author: amerbellioum
-"""
-
 import numpy as np
 import pandas as pd
 
 
 def hedgebook(delta, prices):
     """
-    Vectorised hedgebook with no rebalancing on the final day.
+    Hedgebook with no rebalancing on the final day.
 
     Parameters
     ----------
@@ -35,16 +27,19 @@ def hedgebook(delta, prices):
     n = len(delta)
     money_held = np.zeros(n)
 
-    # Day 0: initial hedge setup
+    # Day 0: Initial Hedge Setup
+    
     hedge_cost = delta[0] * prices[0]
     money_held[0] = hedge_cost
 
-    # Rebalancing from t = 1 to t = n - 2
-    d_delta = delta[1:-1] - delta[:-2]                  # shape: (n-2,)
-    hedge_cost += np.sum(d_delta * prices[1:-1])        # rebalancing costs
-    money_held[1:-1] = delta[1:-1] * prices[1:-1]        # value of hedge
+    # Re-Balancing: t = 1 to t = n - 2
+    
+    d_delta = delta[1:-1] - delta[:-2]                  
+    hedge_cost += np.sum(d_delta * prices[1:-1])       
+    money_held[1:-1] = delta[1:-1] * prices[1:-1]
 
-    # Final day: do not rebalance, use delta from t = n - 2
+    # Final Day: No Rebalancing
+    
     money_held[-1] = delta[-2] * prices[-1]
 
     return hedge_cost, money_held
