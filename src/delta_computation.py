cb3967f5c09410e36cc6dee36c3466f9216270c6
AmerBellioum <abellioum14@gmail.com>
2025-06-17 15:01:38 +0100

    Update delta_computation.py

--- src/delta_computation.py
+++ src/delta_computation.py
@@ -1,21 +1,13 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jun 11 08:20:58 2025
-
-@author: amerbellioum
-"""
-
 import numpy as np
 import pandas as pd
 from blackscholespricer import delta_finder
 
-def delta_computation(data, K, r, vol, option_type="call"):
+def delta_computation(data, K, r, vol, option_type):
     """
     Computes delta for each time step in the data (vectorised).
     
     Parameters:
-        data (DataFrame): Must include 'Close/Last' and 'Date'
+        data (DataFrame): Window of Market Data
         K (float): Strike
         r (float): Risk-free rate
         vol (float): Volatility
@@ -25,8 +17,8 @@ def delta_computation(data, K, r, vol, option_type="call"):
         delta (np.ndarray): Array of deltas for each row in data
     """
     
-    S = data["Close/Last"].to_numpy()                                       # Convert S to NumPy array
-    tau_days = (data["Date"].iloc[-1] - data["Date"]).dt.days.to_numpy()    # Convert tau to NumPy array
-    tau = tau_days / 365                                                    # Time to maturity in years
-
+    S = data["Close/Last"].to_numpy()                                       
+    tau_days = (data["Date"].iloc[-1] - data["Date"]).dt.days.to_numpy()
+    tau = tau_days / 365
+    
     return delta_finder(S, K, r, tau, vol, option_type)
