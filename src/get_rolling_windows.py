03dac066ce08ad9ba60178390a7ccb79fd7ffc80
AmerBellioum <abellioum14@gmail.com>
2025-06-17 14:58:42 +0100

    Update get_rolling_windows.py

--- src/get_rolling_windows.py
+++ src/get_rolling_windows.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jun 10 14:43:45 2025
-
-@author: amerbellioum
-"""
-
 import pandas as pd
 
 def get_rolling_windows(ticker_data, T, rw):
@@ -22,19 +14,16 @@ def get_rolling_windows(ticker_data, T, rw):
             Dictionary of Dataframes corresponding to time periods
     """
     
-    windows = {}                                                                            # Dictionary Initialised
-    ticker_data["Date"] = pd.to_datetime(ticker_data["Date"])                               # Trading Dates Numerised
-    ticker_data = ticker_data.sort_values("Date", ascending=True).reset_index(drop=True)    # Dates Flipped (Start Date to Expiry)
+    windows = {}                                                                            
+    ticker_data = ticker_data.sort_values("Date", ascending=True).reset_index(drop=True)
 
-
-    # While Loop 
-    # -> Creates rolling windows until not enough dates left.
+    # While Loop: Rolling Windows Developed
     
     k = 0 
     while k + T <= len(ticker_data):
-        rolling_series = ticker_data.iloc[k :(k + T)]           # Rolling Series of Data
-        start_date = rolling_series.iloc[0]["Date"]            # Start Date of Window
-        windows[start_date] = rolling_series                    # Appended to Dict.
+        rolling_series = ticker_data.iloc[k :(k + T)]           
+        start_date = rolling_series.iloc[0]["Date"]             
+        windows[start_date] = rolling_series                  
         
         k += rw
         
